3381d463856aaaff60f7a36bad54f0b36871494e
Nicole Eddy <hrsujdgijf@gmail.com>
2023-12-12 02:36:56 -0800

    changed name and first initial commit and final draft of in my opinion blog created by nicole eddy

---
+++ public/images/index.js
@@ -0,0 +1,54 @@
+import backend from './backend.jpg';
+import frontend from './frontend.jpg';
+import fullstack from './fullstack.jpg';
+import nature1 from './nature1.jpg';
+import nature2 from './nature2.jpg';
+import nature3 from './nature3.jpg';
+import nature4 from './nature4.jpg';
+import nature5 from './nature5.jpg';
+import nature6 from './nature6.jpg';
+import nature7 from './nature7.jpg';
+import cartoonself from './cartoonself.jpg';
+import code from './code.jpg';
+import code11 from './code11.jpg';
+import code2 from './code2.jpg';
+import kylar from './kylar.jpg';
+import kylar1 from './kylar1.jpg';
+import nevaeh from './nevaeh.webp';
+import nevaeh1 from './nevaeh1.webp';
+
+import nevaeh3 from './nevaeh3.webp';
+import nevaeh4 from './nevaeh4.webp';
+import nevaeh5 from './nevaeh5.webp';
+import nevaeh6 from './nevaeh6.webp';
+import nevaeh7 from './nevaeh7.webp';
+import logo from './logo.png';
+
+
+export {
+    backend,
+    frontend,
+    fullstack,
+    nature1,
+    nature2,
+    nature3,
+    nature4,
+    nature5,
+    nature6,
+    nature7,
+   cartoonself,
+   code,
+   code2,
+   code11,
+   logo, 
+   nevaeh,
+   nevaeh1, 
+
+   nevaeh3,
+   nevaeh4,
+   nevaeh5,
+   nevaeh6,
+   nevaeh7,
+   kylar,
+   kylar1,
+};
